c1b9fefa2ad54271c4dfe02296cdda51e3508f4e
Jannis Pohlmann <jannis@thegraph.com>
2020-06-12 13:36:38 +0200

    Add some debug info to pure-evm tests

--- test/ethjs-vm.spec.ts
+++ test/ethjs-vm.spec.ts
@@ -1,11 +1,11 @@
-import { Contract, ContractFactory, Wallet, BigNumber } from "ethers";
-import VM from "ethereumjs-vm";
-import { Transaction } from "ethereumjs-tx";
-import Account from "ethereumjs-account";
-import abi from "ethereumjs-abi";
-import { bufferify, getAddressFromPrivateKey, toBN } from "@connext/utils";
+import { Contract, ContractFactory, Wallet, BigNumber } from 'ethers'
+import VM from 'ethereumjs-vm'
+import { Transaction } from 'ethereumjs-tx'
+import Account from 'ethereumjs-account'
+import abi from 'ethereumjs-abi'
+import { bufferify, getAddressFromPrivateKey, toBN } from '@connext/utils'
 // Get artifacts
-import SimpleLinkedTransferApp from "../artifacts/SimpleLinkedTransferApp.json";
+import SimpleLinkedTransferApp from '../artifacts/SimpleLinkedTransferApp.json'
 import {
   getSimpleLinkedTransferInfo,
   encodeState,
@@ -13,69 +13,69 @@ import {
   decodeState,
   provider,
   expect,
-} from "./utils";
-import { promisify } from "util";
-import { hexlify, getAddress } from "ethers/lib/utils";
+} from './utils'
+import { promisify } from 'util'
+import { hexlify, getAddress } from 'ethers/lib/utils'
 
 async function getAccountNonce(vm: VM, pk: string) {
   const account = (await promisify(
     vm.stateManager.getAccount.bind(vm.stateManager)
-  )(bufferify(getAddressFromPrivateKey(pk)))) as Account;
+  )(bufferify(getAddressFromPrivateKey(pk)))) as Account
 
-  return account.nonce;
+  return account.nonce
 }
 
-describe.only("Pure evm with view function", () => {
-  let simpleLinkedTransferApp: Contract;
+describe('Pure evm with view function', () => {
+  let simpleLinkedTransferApp: Contract
 
   // Constants
-  let wallet: Wallet;
-  let state;
-  let action;
-  let functionData;
+  let wallet: Wallet
+  let state
+  let action
+  let functionData
 
   beforeEach(async () => {
     // Deploy contract
-    wallet = (await provider.getWallets())[0];
+    wallet = (await provider.getWallets())[0]
     const deploy = await new ContractFactory(
       SimpleLinkedTransferApp.abi,
       SimpleLinkedTransferApp.bytecode,
       wallet
-    ).deploy();
-    simpleLinkedTransferApp = await deploy.deployed();
+    ).deploy()
+    simpleLinkedTransferApp = await deploy.deployed()
 
     // Setup test constants
-    const res = getSimpleLinkedTransferInfo();
-    state = res.state;
-    action = res.action;
+    const res = getSimpleLinkedTransferInfo()
+    state = res.state
+    action = res.action
     functionData = simpleLinkedTransferApp.interface.encodeFunctionData(
-      "applyAction",
+      'applyAction',
       [encodeState(state), encodeAction(action)]
-    );
-  });
+    )
+  })
 
-  it.only("should be able to call applyAction using eth-js", async () => {
+  it('should be able to call applyAction using eth-js', async () => {
     // Add wallet to VM
-    const vm = new VM();
+    const vm = new VM()
     const account = new Account({
       balance: (await wallet.getBalance()).toHexString(),
-    });
-    vm.stateManager.putAccount(bufferify(wallet.address), account, () => {});
+    })
+    vm.stateManager.putAccount(bufferify(wallet.address), account, () => {})
 
     // Deploy contract to vm
-    const bytecode = SimpleLinkedTransferApp.bytecode;
+    const bytecode = SimpleLinkedTransferApp.bytecode
     const deployTx = new Transaction({
       gasLimit: toBN(2000000).toHexString(),
       gasPrice: toBN(1).toHexString(),
       nonce: await getAccountNonce(vm, wallet.privateKey),
       data: bytecode,
-    });
-    deployTx.sign(bufferify(wallet.privateKey));
-    const deploymentResult = await vm.runTx({ tx: deployTx });
-    expect(deploymentResult.execResult.exceptionError).to.be.undefined;
+    })
+    deployTx.sign(bufferify(wallet.privateKey))
+    const deploymentResult = await vm.runTx({ tx: deployTx })
+    expect(deploymentResult.execResult.exceptionError).to.be.undefined
 
-    const appDef = getAddress(hexlify(deploymentResult.createdAddress!));
-    expect(appDef).to.be.eq(simpleLinkedTransferApp.address);
+    const appDef = getAddress(hexlify(deploymentResult.createdAddress!))
+    expect(appDef).to.be.eq(simpleLinkedTransferApp.address)
 
     // Execute on evm
     const actionTx = new Transaction({
@@ -84,27 +84,27 @@ describe.only("Pure evm with view function", () => {
       gasPrice: toBN(1).toHexString(),
       data: functionData,
       nonce: await getAccountNonce(vm, wallet.privateKey),
-    });
-    actionTx.sign(bufferify(wallet.privateKey));
-    const actionResult = await vm.runTx({ tx: actionTx });
-    expect(actionResult.execResult.exceptionError).to.be.undefined;
+    })
+    actionTx.sign(bufferify(wallet.privateKey))
+    const actionResult = await vm.runTx({ tx: actionTx })
+    expect(actionResult.execResult.exceptionError).to.be.undefined
     const vmEncoded = simpleLinkedTransferApp.interface.decodeFunctionResult(
-      "applyAction",
+      'applyAction',
       hexlify(actionResult.execResult.returnValue)
-    );
+    )
 
     // Execute on contract
     const encoded = await simpleLinkedTransferApp.functions.applyAction(
       encodeState(state),
       encodeAction(action)
-    );
+    )
 
-    expect(encoded[0]).to.eq(vmEncoded[0]);
+    expect(encoded[0]).to.eq(vmEncoded[0])
 
     // Decode outputs
-    const contractDecoded = decodeState(encoded[0]);
-    const vmDecoded = decodeState(vmEncoded[0]);
+    const contractDecoded = decodeState(encoded[0])
+    const vmDecoded = decodeState(vmEncoded[0])
     // Verify both values are the same once decoded
-    expect(vmDecoded).to.containSubset(contractDecoded);
-  });
-});
+    expect(vmDecoded).to.containSubset(contractDecoded)
+  })
+})
--- test/pure-evm.spec.ts
+++ test/pure-evm.spec.ts
@@ -1,8 +1,8 @@
-import { Contract, ContractFactory } from "ethers";
-const pure_evm = require("pure-evm");
+import { Contract, ContractFactory } from 'ethers'
+const pure_evm = require('pure-evm-wasm')
 
 // Get artifacts
-import SimpleLinkedTransferApp from "../artifacts/SimpleLinkedTransferApp.json";
+import SimpleLinkedTransferApp from '../artifacts/SimpleLinkedTransferApp.json'
 import {
   getSimpleLinkedTransferInfo,
   encodeState,
@@ -10,88 +10,111 @@ import {
   decodeState,
   provider,
   expect,
-} from "./utils";
-import { defaultAbiCoder } from "ethers/lib/utils";
+} from './utils'
+import { defaultAbiCoder } from 'ethers/lib/utils'
 
-describe("Pure evm with view function", () => {
-  let simpleLinkedTransferApp: Contract;
+describe('Pure evm with view function', () => {
+  let simpleLinkedTransferApp: Contract
 
   // Constants
-  let formatted;
-  let functionData;
-  let state;
-  let action;
+  let formatted
+  let functionData
+  let state
+  let action
 
   beforeEach(async () => {
     // Deploy contract
-    const wallet = (await provider.getWallets())[0];
+    const wallet = (await provider.getWallets())[0]
     const deploy = await new ContractFactory(
       SimpleLinkedTransferApp.abi,
       SimpleLinkedTransferApp.bytecode,
       wallet
-    ).deploy();
-    simpleLinkedTransferApp = await deploy.deployed();
+    ).deploy()
+    simpleLinkedTransferApp = await deploy.deployed()
 
     // Setup test constants
-    const res = getSimpleLinkedTransferInfo();
-    state = res.state;
-    action = res.action;
+    const res = getSimpleLinkedTransferInfo()
+    state = res.state
+    action = res.action
 
     // Get the function data
     functionData = simpleLinkedTransferApp.interface.encodeFunctionData(
-      "applyAction",
+      'applyAction',
       [encodeState(state), encodeAction(action)]
-    );
+    )
 
     // Format for evm execution
     formatted = Uint8Array.from(
-      Buffer.from(functionData.replace("0x", ""), "hex")
-    );
-  });
+      Buffer.from(functionData.replace('0x', ''), 'hex')
+    )
+  })
 
-  it("should fetch the deployed bytecode using rpc call", async () => {
+  it('should fetch the deployed bytecode using rpc call', async () => {
     // Verify rpc will return bytecode
-    const bytecode = await provider.send("eth_getCode", [
+    const bytecode = await provider.send('eth_getCode', [
       simpleLinkedTransferApp.address,
-      "latest",
-    ]);
-    expect(bytecode).to.equal(SimpleLinkedTransferApp.deployedBytecode);
-  });
+      'latest',
+    ])
+    expect(bytecode).to.equal(SimpleLinkedTransferApp.deployedBytecode)
+  })
 
-  it("returns deployed bytecode if executed using init code", async () => {
+  it('returns deployed bytecode if executed using init code', async () => {
     // Execute on evm, decode output
     const output = pure_evm.exec(
       Uint8Array.from(
-        Buffer.from(SimpleLinkedTransferApp.bytecode.replace("0x", ""), "hex")
+        Buffer.from(SimpleLinkedTransferApp.bytecode.replace('0x', ''), 'hex')
       ),
       formatted
-    );
-    const outputHex = "0x" + Buffer.from(output).toString("hex");
-    expect(outputHex).to.be.eq(SimpleLinkedTransferApp.deployedBytecode);
-  });
+    )
+    const outputHex = '0x' + Buffer.from(output).toString('hex')
+    expect(outputHex).to.be.eq(SimpleLinkedTransferApp.deployedBytecode)
+  })
 
-  it("should be able to call applyAction using pure-evm", async () => {
+  it('should be able to call applyAction using pure-evm', async () => {
+    // Input data
+    console.log('INPUT:')
+    for (let i = 0; i < formatted.length; i += 32) {
+      console.log(
+        ' ',
+        Buffer.from(formatted.slice(i, i + 32))
+          .toString('hex')
+          .replace('0x', '')
+      )
+    }
+
     // Execute on evm, decode output
     const output = pure_evm.exec(
       Uint8Array.from(
         Buffer.from(
-          SimpleLinkedTransferApp.deployedBytecode.replace("0x", ""),
-          "hex"
+          SimpleLinkedTransferApp.deployedBytecode.replace('0x', ''),
+          'hex'
         )
       ),
       formatted
-    );
-    const bytes = defaultAbiCoder.decode(["bytes"], output)[0];
-    const evmDecoded = decodeState(bytes);
+    )
+
+    console.log('OUTPUT:')
+    for (let i = 0; i < output.length; i += 32) {
+      console.log(
+        ' ',
+        Buffer.from(output.slice(i, i + 32))
+          .toString('hex')
+          .replace('0x', '')
+      )
+    }
+
+    const outputValues = defaultAbiCoder.decode(['bytes'], output)
+    console.log('OUTPUT VALUES:', outputValues)
+    const evmDecoded = decodeState(outputValues[1])
 
     // Execute on contract, decode output
     const encoded = await simpleLinkedTransferApp.functions.applyAction(
       encodeState(state),
       encodeAction(action)
-    );
-    const contractDecoded = decodeState(encoded);
+    )
+    const contractDecoded = decodeState(encoded)
 
     // Verify both values are the same once decoded
-    expect(evmDecoded).to.containSubset(contractDecoded);
-  });
-});
+    expect(evmDecoded).to.containSubset(contractDecoded)
+  })
+})
